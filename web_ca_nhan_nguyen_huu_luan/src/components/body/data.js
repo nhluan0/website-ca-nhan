f9727882145ae4d919ef0ffa21604de34a903498
luan nguyen <nhluan0@gmail.com>
2024-09-20 22:26:30 +0700

    add lan 8

--- web_ca_nhan_nguyen_huu_luan/src/components/body/data.js
+++ web_ca_nhan_nguyen_huu_luan/src/components/body/data.js
@@ -95,6 +95,6 @@ export const dataProject = [
     Frontend: 'react js, react hook, tailwind css...',
     Backend: '',
     github: 'https://github.com/nhluan0/website-ca-nhan',
-    linkWeb: 'https://nguyenhuuluancv2.netlify.app',
+    linkWeb: 'https://nguyenhuuluancv3.netlify.app',
   },
 ]
